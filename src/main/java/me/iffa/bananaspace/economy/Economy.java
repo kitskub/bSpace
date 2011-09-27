3dc7804774d31ecf6aa4a473bc89e6adfc25dd39
Santtu Elo <iffamies1337@hotmail.fi>
2011-09-27 20:04:30 +0300

    kitskubs implementation, my standards.

--- src/main/java/me/iffa/bananaspace/economy/Economy.java
+++ src/main/java/me/iffa/bananaspace/economy/Economy.java
@@ -1,97 +1,168 @@
+// Package Declaration
 package me.iffa.bananaspace.economy;
 
+// Register Imports
+import com.nijikokun.register.payment.Method;
 import com.nijikokun.register.payment.Method.MethodAccount;
+import com.nijikokun.register.payment.Methods;
+
+// BananaSpace Imports
 import me.iffa.bananaspace.BananaSpace;
 import me.iffa.bananaspace.config.SpaceConfig;
+
+// Bukkit Imports
 import org.bukkit.entity.Player;
-import com.nijikokun.register.payment.*;
 
 /**
- *
+ * Handles economy using Register.
+ * 
  * @author kitskub
+ * @author iffa
  */
 public class Economy {
+    // Variables
     private static Method method;
     private static BananaSpace plugin;
     private boolean use;
 
     /**
-     * currently only supports iconomy
-     * @param plugin
+     * Constructor of Economy.
+     * 
+     * @param plugin BananaSpace instance
      */
     public Economy(BananaSpace plugin) {
-        this.plugin = plugin;
-        use=true;
+        Economy.plugin = plugin;
+        use = true;
         getMethod();
-        BananaSpace.debugLog("Hooked into "+method.getName());
+        BananaSpace.debugLog("Hooked into " + method.getName());
     }
+    
     /**
-     * 
+     * Constructor of Economy #2.
      */
-    public Economy(){
-        use=false;
-    }
-    public static boolean checkEconomy(BananaSpace plugin){
-            if(SpaceConfig.getConfig().getBoolean("economy.enabled", true)){
-                return (getMethod()!=null);
-            }
-        return false;
-    }
-    public boolean enter(Player player) {
-        if (use==false) return true;
-            if(method.hasAccount(player.getName())) {
-                int amount =SpaceConfig.getConfig().getInt("economy.entercost",20);
-                return subtract(player,amount);
-            } else {
-                return false;
-            }   
-            
-            
-         
-    }
-    public boolean exit(Player player){
-        if (use==false) return true;
-            if(method.hasAccount(player.getName())) {
-                int amount =SpaceConfig.getConfig().getInt("economy.exitcost",20);
-                return subtract(player,amount);
-            } else {
-                return false;
-            }   
-        
-    }
-    public boolean enterCommand(Player player){
-        if (use==false) return true;
-            if(method.hasAccount(player.getName())) {
-                int amount =SpaceConfig.getConfig().getInt("economy.entercommandcost",20);
-                return subtract(player,amount);
-            } else {
-                return false;
-            }   
-         
-    }
-    public boolean exitCommand(Player player){
-        if (use==false) return true;
-            if(method.hasAccount(player.getName())) {
-                int amount =SpaceConfig.getConfig().getInt("economy.exitcommandcost",20);
-                return subtract(player,amount);
-            } else {
-                return false;
-            }   
+    public Economy() {
+        use = false;
     }
 
+    /**
+     * Checks the economy enabled-state of the plugin.
+     * 
+     * @param plugin BananaSpace instance
+     * 
+     * @return True if economy is enabled
+     */
+    public static boolean checkEconomy(BananaSpace plugin) {
+        if (SpaceConfig.getConfig().getBoolean("economy.enabled", true)) {
+            return (getMethod() != null);
+        }
+        return false;
+    }
+
+    /**
+     * Checks the cost for entering (general).
+     * 
+     * @param player Player
+     * 
+     * @return True if the player has enough money to enter
+     */
+    public boolean enter(Player player) {
+        if (use == false) {
+            return true;
+        }
+        if (method.hasAccount(player.getName())) {
+            int amount = SpaceConfig.getConfig().getInt("economy.entercost", 20);
+            return subtract(player, amount);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Checks the cost for exiting (general).
+     * 
+     * @param player Player
+     * 
+     * @return True if the player has enough money to exit 
+     */
+    public boolean exit(Player player) {
+        if (use == false) {
+            return true;
+        }
+        if (method.hasAccount(player.getName())) {
+            int amount = SpaceConfig.getConfig().getInt("economy.exitcost", 20);
+            return subtract(player, amount);
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Checks the cost for entering with the command.
+     * 
+     * @param player Player
+     * 
+     * @return True if the player has enough money to enter 
+     */
+    public boolean enterCommand(Player player) {
+        if (use == false) {
+            return true;
+        }
+        if (method.hasAccount(player.getName())) {
+            int amount = SpaceConfig.getConfig().getInt("economy.entercommandcost", 20);
+            return subtract(player, amount);
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Checks the cost for exiting with the command.
+     * 
+     * @param player Player
+     * 
+     * @return True if the player has enough money to exit
+     */
+    public boolean exitCommand(Player player) {
+        if (use == false) {
+            return true;
+        }
+        if (method.hasAccount(player.getName())) {
+            int amount = SpaceConfig.getConfig().getInt("economy.exitcommandcost", 20);
+            return subtract(player, amount);
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Subtracts from a player's balance if possible.
+     * 
+     * @param player Player to subtract from
+     * @param amount Amount to subtract
+     * 
+     * @return True if subtract was successful
+     */
     private boolean subtract(Player player, int amount) {
-        if(BananaSpace.getPlayerHandler().hasPermission("bananspace.economy.exempt",player)) return true;
+        if (BananaSpace.getPlayerHandler().hasPermission("bananspace.economy.exempt", player)) {
+            return true;
+        }
         MethodAccount balance = method.getAccount(player.getName());
-        if(!balance.hasEnough(amount)) return false;
+        if (!balance.hasEnough(amount)) {
+            return false;
+        }
         balance.subtract(amount);
         return true;
     }
-
+    
     /**
-     * @return method
+     * Gets the payment method.
+     * 
+     * @return Payment method
      */
     public static Method getMethod() {
-        if(method==null){
+        if (method == null) {
             Methods.setMethod(plugin.getServer().getPluginManager());
             method = Methods.getMethod();
         }
