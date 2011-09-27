d42510d3162a52c112aef7da92aa8b1dde9ad6dc
Santtu Elo <iffamies1337@hotmail.fi>
2011-09-27 20:16:21 +0300

    Dependencies, dependencies... MOAR DEPENDENCIES!

---
+++ src/main/java/me/iffa/bananaspace/api/SpaceMessageHandler.java
@@ -0,0 +1,11 @@
+// Package Declaration
+package me.iffa.bananaspace.api;
+
+/**
+ * Useful methods to send messages to players and console.
+ * 
+ * @author iffa
+ */
+public class SpaceMessageHandler {
+    
+}
