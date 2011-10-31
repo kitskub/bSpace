3031246f5edbd7abc582df10519d570f7ac8cd4d
Santtu Elo <iffamies1337@hotmail.fi>
2011-10-31 21:17:30 +0200

    Updated CallHome.java

--- src/main/java/me/iffa/bananaspace/BananaSpace.java
+++ src/main/java/me/iffa/bananaspace/BananaSpace.java
@@ -41,7 +41,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 // BukkitStats Imports
-import org.blockface.bukkitstats.CallHome;
+import org.blockface.stats.CallHome;
 
 /**
  * Main class of BananaSpace.
